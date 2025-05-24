86d0e1420e415bce27f6beca5a832c5f5c192773
rafiqwe <rmlmanik502@gmail.com>
2025-05-24 21:51:10 +0600

    Add pagenation using react-query

--- src/APIS/api.jsx
+++ src/APIS/api.jsx
@@ -3,6 +3,6 @@ import axios from "axios";
 const api = axios.create({
     baseURL: "https://jsonplaceholder.typicode.com",
 });
-export const getPosts = () => {
-    return api.get("/posts");
+export const getPosts = (pageNumber) => {
+    return api.get(`/posts?_start=${pageNumber}&_limit=10`);
 }
--- src/Pages/FetchRQ.jsx
+++ src/Pages/FetchRQ.jsx
@@ -1,19 +1,24 @@
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { getPosts } from "../APIS/api";
+import { useState } from "react";
 
 export const FetchRQ = () => {
-  const getApiData = async () => {
+  const [pageNumber, setPageNumber] = useState(0);
+  const getApiData = async (pageNumber) => {
     try {
-      const res = await getPosts();
-     return res.status === 200 && res.data;
+      const res = await getPosts(pageNumber);
+      return res.status === 200 && res.data;
     } catch (error) {
       console.log(error);
     }
   };
+
   const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["posts"],
-    queryFn: getApiData,
+    queryKey: ["posts", pageNumber],
+    queryFn: () => getApiData(pageNumber),
+    placeholderData: keepPreviousData,
   });
+
   if (isLoading) return <div className="text-center mt-10">Loading...</div>;
   if (isError)
     return (
@@ -21,6 +26,7 @@ export const FetchRQ = () => {
         Error: {error.message}
       </div>
     );
+    
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-[90%] mx-auto mt-10 max-w-[1920px] mb-10">
@@ -34,6 +40,23 @@ export const FetchRQ = () => {
             <span className="text-sm text-gray-500">Post ID: {item.id}</span>
           </div>
         ))}
+      </div>
+      <div className="pagenation flex justify-center items-center gap-4 mb-10">
+        <button
+          className="text-white font-bold bg-gray-800 px-9 py-1 rounded-lg"
+          onClick={() => setPageNumber((prev) => Math.max(prev - 10, 0))}
+        >
+          Prev
+        </button>
+        <h2 className="text-green-700 font-bold text-xl">
+          {pageNumber / 10 + 1}
+        </h2>
+        <button
+          className="text-white font-bold bg-gray-800 px-9 py-1 rounded-lg"
+          onClick={() => setPageNumber((prev) => prev + 10)}
+        >
+          Next
+        </button>
       </div>
     </>
   );
