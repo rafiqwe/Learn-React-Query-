7bc64440baac5de5543e041c48378c12d4be2dd9
rafiqwe <rmlmanik502@gmail.com>
2025-05-24 16:59:33 +0600

    Add style in error page component

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,7 +1,21 @@
+import { useNavigation } from "react-router-dom";
+
 export const ErrorPage = () => {
-    return(
-        <>
-            <h1>This is Error Page</h1>
-        </>
-    )
-}
+  const navigation = useNavigation();
+  return (
+    <>
+      {navigation.state === "loading" ? (
+        <div className="flex justify-center items-center h-screen">
+          <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-blue-500"></div>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+          <p className="text-lg">
+            The page you are looking for does not exist.
+          </p>
+        </div>
+      )}
+    </>
+  );
+};
